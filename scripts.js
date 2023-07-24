c1354f2fb22e43f889d086529fd8bf50c266f6e2
nightshifty <35473202+nightshifty@users.noreply.github.com>
2023-07-24 20:13:36 +0200

    move shorts area

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Hide Youtube Shorts Mobile
+// @name         Youtube Shorts Mobile killer
 // @version      0.1
 // @description  Removes Shorts in the bottom bar of youtube mobile
 // @author       nightshifty
@@ -15,12 +15,23 @@
         document.querySelectorAll('ytm-pivot-bar-item-renderer').forEach(t => {
             if (t.innerText === "Shorts") {
                 t.remove();
-                console.log('Removed shorts section');
+                console.log('Removed shorts tab');
+                }
+            }
+        );
+
+        document.querySelectorAll('ytm-item-section-renderer').forEach(t => {
+            if (t.innerText.match("Shorts*")) {
+                t.remove();
+                console.log('Removed shorts area');
                 }
             }
         );
     }
 
+
+
     const observer = new MutationObserver(removeShorts);
     observer.observe(document.querySelector('ytm-pivot-bar-renderer'), { childList: true, subtree: true });
+
 })();
